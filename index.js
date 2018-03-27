fddfc863b8e1c6f6d9e2b93b46865a438efa4c96
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-27 11:34:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+let store = {drivers: [], passengers: [], trips: []}
+let driverId = 0;
+
+class Driver {
+  constructor(name) {
+    this.name = name;
+    this.id = ++driverId;
+
+    store.drivers.push(this)
+  }
+}
+
+let passengerId = 0;
+
+class Passenger {
+  constructor(name) {
+    this.name = name;
+    this.id = ++passengerId;
+
+    store.passengers.push(this)
+  }
+}
+
+let tripId = 0;
+
+class Trip {
+  constructor(driver, passenger) {
+    this.driverId = driver;
+    this.passengerId = passenger;
+    this.id = ++tripId;
+
+    store.trips.push(this)
+  }
+}
