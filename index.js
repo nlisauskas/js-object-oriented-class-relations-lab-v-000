d0b21f12d30138d1301e28cf94eaea28a693fecf
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-31 00:43:41 +0000

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ class Driver {
 
     store.drivers.push(this)
   }
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId === this.id;
+    });
+  }
+  passengers() {
+    let trips = this.trips();
+    return trips.map(trip => {
+      return trip.passenger();
+    });
+  }
 }
 
 let passengerId = 0;
@@ -19,16 +30,37 @@ class Passenger {
 
     store.passengers.push(this)
   }
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId === this.id;
+    });
+  }
+  drivers() {
+    let trips = this.trips();
+    return trips.map(trip => {
+      return trip.driver();
+    });
+  }
 }
 
 let tripId = 0;
 
 class Trip {
   constructor(driver, passenger) {
-    this.driverId = driver;
-    this.passengerId = passenger;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
     this.id = ++tripId;
 
     store.trips.push(this)
+  }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
   }
 }
